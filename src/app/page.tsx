e1b2305539b6b5f20ef2886c8b2fe67a9f5866ce
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-05-28 01:49:52 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome to the Home Page</h1>
+      <Button>Click Me</Button>
     </div>
-  );
+  )
 }
+
+export default Home
